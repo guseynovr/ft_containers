7ed8a69c70c98f597d16d41f6edd3ce61dd4e379
Ruslan Guseynov <rslnguseynov@gmail.com>
2022-06-18 09:06:32 +0300

    ...

--- vector.hpp
+++ vector.hpp
@@ -1,10 +1,36 @@
 
 namespace ft {
 
-
 template < class T, class Alloc = allocator<T> >
 class vector {
 
+    template< class T, class Alloc >
+        bool operator==( const std::vector<T,Alloc>& lhs,
+                        const std::vector<T,Alloc>& rhs );
+
+    template< class T, class Alloc >
+    constexpr bool operator==(const std::vector<T,Alloc>& lhs,
+                              const std::vector<T,Alloc>& rhs );
+
+    template< class T, class Alloc >
+    bool operator!=(const std::vector<T,Alloc>& lhs,
+                    const std::vector<T,Alloc>& rhs );
+
+    template< class T, class Alloc >
+    bool operator<(const std::vector<T,Alloc>& lhs,
+                   const std::vector<T,Alloc>& rhs );
+
+    template< class T, class Alloc >
+    bool operator<=(const std::vector<T,Alloc>& lhs,
+                    const std::vector<T,Alloc>& rhs );
+
+    template< class T, class Alloc >
+    bool operator>(const std::vector<T,Alloc>& lhs,
+                   const std::vector<T,Alloc>& rhs );
+
+    template< class T, class Alloc >
+    bool operator>=(const std::vector<T,Alloc>& lhs,
+                    const std::vector<T,Alloc>& rhs );
 };
 
 } //namespace ft
