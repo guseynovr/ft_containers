95565f93c808f31427a58d6d195a2a52fa3f293a
guseynov <rslnguseynov@gmail.com>
2022-06-19 05:08:05 +0300

    start vector

--- tests/README.md
+++ tests/README.md
@@ -2,4 +2,8 @@
 In order to run tests:
 1. Download googletest and prepare the build system: `cmake -S . -B build`
 2. Run the build system and wait for it to complete: `cmake --build build`
-3. Run tests: `./build/stl_tests`
+3. Run tests: `./build/stl_test`
+
+To run selected tests:
+1. List all tests: `./build/stl_test --gtest_list_tests`
+2. Run and select tests:  `./build/stl_test --gtest_filter=...[*]`
--- vector.hpp
+++ vector.hpp
@@ -1,36 +1,35 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <memory>
+#include <vector>
 
 namespace ft {
+template <class T, class Allocator = std::allocator<T> >
+class vector
+{
+public:
+    typedef T                                        value_type;
+    typedef Allocator                                allocator_type;
+    typedef typename allocator_type::reference       reference;
+    typedef typename allocator_type::const_reference const_reference;
+    typedef typename allocator_type::pointer         pointer;
+    typedef typename allocator_type::const_pointer   const_pointer;
+    typedef implementation-defined                   iterator;
+    typedef implementation-defined                   const_iterator;
+    typedef ft::reverse_iterator<iterator>           reverse_iterator;
+    typedef ft::reverse_iterator<const_iterator>     const_reverse_iterator;
+    typedef typename allocator_type::difference_type difference_type;
+    typedef std::size_t                              size_type;
 
-template < class T, class Alloc = allocator<T> >
-class vector {
-
-    template< class T, class Alloc >
-        bool operator==( const std::vector<T,Alloc>& lhs,
-                        const std::vector<T,Alloc>& rhs );
-
-    template< class T, class Alloc >
-    constexpr bool operator==(const std::vector<T,Alloc>& lhs,
-                              const std::vector<T,Alloc>& rhs );
-
-    template< class T, class Alloc >
-    bool operator!=(const std::vector<T,Alloc>& lhs,
-                    const std::vector<T,Alloc>& rhs );
-
-    template< class T, class Alloc >
-    bool operator<(const std::vector<T,Alloc>& lhs,
-                   const std::vector<T,Alloc>& rhs );
-
-    template< class T, class Alloc >
-    bool operator<=(const std::vector<T,Alloc>& lhs,
-                    const std::vector<T,Alloc>& rhs );
-
-    template< class T, class Alloc >
-    bool operator>(const std::vector<T,Alloc>& lhs,
-                   const std::vector<T,Alloc>& rhs );
-
-    template< class T, class Alloc >
-    bool operator>=(const std::vector<T,Alloc>& lhs,
-                    const std::vector<T,Alloc>& rhs );
+    bool operator==(const std::vector<T,Alloc>& lhs, const std::vector<T,Alloc>& rhs);
+    bool operator!=(const std::vector<T,Alloc>& lhs, const std::vector<T,Alloc>& rhs);
+    bool operator< (const std::vector<T,Alloc>& lhs, const std::vector<T,Alloc>& rhs);
+    bool operator<=(const std::vector<T,Alloc>& lhs, const std::vector<T,Alloc>& rhs);
+    bool operator> (const std::vector<T,Alloc>& lhs, const std::vector<T,Alloc>& rhs);
+    bool operator>=(const std::vector<T,Alloc>& lhs, const std::vector<T,Alloc>& rhs);
 };
 
-} //namespace ft
+} // namespace ft
+
+#endif // VECTOR_H
