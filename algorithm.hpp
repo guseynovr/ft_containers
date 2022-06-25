c0568bc554b386bf14c7cc6cc81f03af155245aa
Doreen Gidget <dgidget@pr-c5.kzn.21-school.ru>
2022-06-25 16:46:37 +0300

    add lexicographical_compare overload

--- algorithm.hpp
+++ algorithm.hpp
@@ -1,6 +1,8 @@
 #ifndef ALGORITHM_H
 #define ALGORITHM_H
 
+#include <algorithm>
+
 namespace ft {
 
 template <class T>
@@ -15,12 +17,18 @@ template <class InputIterator1, class InputIterator2>
 inline bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
                                      InputIterator2 first2, InputIterator2 last2)
 {
-    while (first1!=last1) {
-        if (first2==last2 || *first2<*first1) return false;
-        else if (*first1<*first2) return true;
-        ++first1; ++first2;
+    // while (first1!=last1) {
+    //     if (first2==last2 || *first2<*first1) return false;
+    //     else if (*first1<*first2) return true;
+    //     ++first1; ++first2;
+    // }
+    // return (first2!=last2);
+    for (; first1 != last1 && first2 != last2; ++first1, (void)++first2)
+    {
+        if (*first1 < *first2) return true;
+        if (*first2 < *first1) return false;
     }
-    return (first2!=last2);
+    return first1 == last1 && first2 != last2;
 }
 
 template <class InputIterator1, class InputIterator2, class Compare>
@@ -28,7 +36,12 @@ inline bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1
                                      InputIterator2 first2, InputIterator2 last2,
                                      Compare comp)
 {
-
+    for (; first1 != last1 && first2 != last2; ++first1, (void)++first2)
+    {
+        if (comp(*first1, *first2)) return true;
+        if (comp(*first2, *first1)) return false;
+    }
+    return first1 == last1 && first2 != last2;
 }
 
 }; // namescape ft
